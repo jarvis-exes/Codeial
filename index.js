beeda9e2d83088976ca7dbc7fae0f44543da6f8c
Shubham <8696771553shubham@gmail.com>
2023-06-07 14:03:15 +0530

    Added Layouts and Routes

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// Using express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
     }
-
     console.log(`Server is Running on port : ${port}`);    
 });
