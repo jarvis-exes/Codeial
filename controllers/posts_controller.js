b5698f863faf2807a9fe66b66e719a012b3304ae
Shubham <8696771553shubham@gmail.com>
2023-07-01 19:58:59 +0530

    Converted some features to AJAX

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -17,11 +17,20 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try {
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id    
         });
-        
+
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post
+                },
+                messsage : 'Post Created !'
+            });
+        }
+        req.flash('success', 'Post Published');
         return res.redirect('back');
 
     } catch (err) {
@@ -38,6 +47,17 @@ module.exports.destroy = async function(req, res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: 'Post Deleted'
+                });
+            }
+
+            req.flash('success', 'Post is deleted');
             return res.redirect('back');
             
         }else{
