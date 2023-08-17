e8b0859b03d8fd94ae22afb7b2adbbd9e4c6741d
Shubham <8696771553shubham@gmail.com>
2023-08-17 09:24:58 +0530

    Added Node Mailer

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,20 +1,6 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-//Function without async
-
-// module.exports.create = function(req, res){
-//     Post.create({
-//         content: req.body.content,
-//         user: req.user._id
-//     }, function(err, post){
-//         if(err){
-//             console.log('Error in creating the post'); return;
-//         }
-//         return res.redirect('back');
-//     });
-// }
-
 module.exports.create = async function(req, res){
     try {
         let post = await Post.create({
---
+++ mailers/comments_mailer.js
@@ -0,0 +1,21 @@
+const nodeMailer = require('../config/nodemailer');
+
+//This is another way of exporting a method
+exports.newComment = (comment) => {
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+    nodeMailer.transporter.sendMail({
+        from: 'rj13sgnr01@gmail.com',
+        to: comment.user.email,
+        subject: 'New Comment Published',
+        html: htmlString
+    }, (err, info) => {
+        if(err){
+            console.log('Error in sending mail', err);
+            return;
+        }
+
+        console.log('Message sent', info);
+        return;
+    });
+}
