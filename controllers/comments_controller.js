7898cc26bc00408d63e803a39123d7f722c75dd5
Shubham <8696771553shubham@gmail.com>
2023-08-18 21:38:45 +0530

    Added Kue and Mailers

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker =  require('../workers/comment_email_worker');
 
 module.exports.create = async function(req, res){
     try{
@@ -18,7 +20,14 @@ module.exports.create = async function(req, res){
             
             // comment = await comment.populate('user', 'name email').execPopulate();
             comment = await comment.populate(['user']);
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+            let job = queue.create('emails', comment).save(function(err){
+                if(err){
+                    console.log('Error in creating queue');
+                    return;
+                }
+                console.log('Job Enqueued',job.id);
+            });
 
             if (req.xhr){
                 return res.status(200).json({
